8c660cfcb998bbd45c2f1d2b5c072d021a01f3a7
ayka98i <ayka98i@dmail.ai>
2022-11-03 18:31:23 +0900

    update: output

--- src/files.ts
+++ src/files.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import { ethers } from 'ethers';
 
 export function appendFile(wallet: ethers.Wallet, balance: ethers.BigNumber) {
-  fs.appendFile('./result.csv', wallet.privateKey + ',' + balance._hex, (err) => {
+  fs.appendFile('./result.csv', wallet.privateKey + ',' + balance.toNumber(), (err) => {
     if (err) {
       throw new Error(err.message);
     }
